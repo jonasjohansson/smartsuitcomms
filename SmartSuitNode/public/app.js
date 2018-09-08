9c2d677af868e4786aa2e3142c7737508e5a666e
Jonas Johansson <jns.johansson@gmail.com>
2018-09-08 18:36:36 +0200

    Added more joints!

--- SmartSuitNode/public/app.js
+++ SmartSuitNode/public/app.js
@@ -9,18 +9,42 @@ window.onload = () => {
 	const c = 10;
 
 	let joints = [
+		'RokokoGuy_Hips',
+
+		'RokokoGuy_LeftUpLeg',
+		'RokokoGuy_RightUpLeg',
+		'RokokoGuy_Spine',
+
+		'RokokoGuy_LeftLeg',
+		'RokokoGuy_LeftFoot',
+		'RokokoGuy_LeftHeel',
+		'RokokoGuy_LeftToe',
+
+		'RokokoGuy_RightLeg',
+		'RokokoGuy_RightFoot',
+		'RokokoGuy_RightHeel',
+		'RokokoGuy_RightToe',
+
+		'RokokoGuy_Spine',
+		'RokokoGuy_Neck',
 		'RokokoGuy_Head',
+
 		'RokokoGuy_LeftShoulder',
 		'RokokoGuy_LeftArm',
+		'RokokoGuy_LeftForeArm',
+		'RokokoGuy_LeftHand',
+
 		'RokokoGuy_RightShoulder',
 		'RokokoGuy_RightArm',
+		'RokokoGuy_RightForeArm',
+		'RokokoGuy_RightHand',
 		];
 
 	let objects = [];
 
 	for (let joint of joints){
-		var newEl = document.createElement('a-box');
-		newEl.setAttribute('color', 'red');
+		var newEl = document.createElement('a-sphere');
+		newEl.setAttribute('color', 'black');
 		newEl.setAttribute('width', '0.5');
 		newEl.setAttribute('height', '0.5');
 		newEl.setAttribute('depth', '0.5');
